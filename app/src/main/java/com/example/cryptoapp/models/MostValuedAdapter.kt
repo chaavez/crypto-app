96a7f074aadaa7fdf37850fe40a323f35a6ce90f
Anderson <anderson.c-morais@hotmail.com>
2023-03-18 16:04:07 -0300

    Add image view asset to card

--- app/src/main/java/com/example/cryptoapp/models/MostValuedAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/models/MostValuedAdapter.kt
@@ -3,6 +3,7 @@ package com.example.cryptoapp.models
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.cryptoapp.databinding.AssetViewHolderBinding
 
 class MostValuedAdapter(private val assets : List<Asset> = ArrayList()) : RecyclerView.Adapter<AssetViewHolder>() {
@@ -27,6 +28,7 @@ class MostValuedAdapter(private val assets : List<Asset> = ArrayList()) : Recycl
 
 class AssetViewHolder constructor(private val binding: AssetViewHolderBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(asset: Asset) {
+        Glide.with(this.binding.iconImageView).load(asset.icon)
         binding.symbolTextView.text = asset.symbol
         binding.nameTextView.text = asset.name
         binding.priceTextView.text = asset.price.toString()

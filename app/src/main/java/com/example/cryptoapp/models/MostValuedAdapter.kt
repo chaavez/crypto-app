10df1a6e55ce3773fd7e49133d646801c96994eb
Anderson <anderson.c-morais@hotmail.com>
2023-03-21 14:04:02 -0300

    Created logic for percentage values to change color according to the value and only two decimal places appear in currency values

--- app/src/main/java/com/example/cryptoapp/models/MostValuedAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/models/MostValuedAdapter.kt
@@ -2,11 +2,13 @@ package com.example.cryptoapp.models
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.AssetViewHolderBinding
+import java.text.DecimalFormat
 
 class MostValuedAdapter(private var assets : List<Asset> = ArrayList()) : RecyclerView.Adapter<AssetViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AssetViewHolder {
@@ -39,13 +41,26 @@ class AssetViewHolder constructor(private val binding: AssetViewHolderBinding) :
             .placeholder(R.drawable.ic_launcher_background)
             .error(R.drawable.ic_launcher_background)
 
+        val priceValue = asset.price
+        val decimalFormatted = DecimalFormat("#.##")
+        val priceFormatted = decimalFormatted.format(priceValue)
+
+        val variationValue = asset.variation
+        if(variationValue > 0) {
+            binding.variationTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.green_100))
+        } else if(variationValue.equals(0.0)) {
+            binding.variationTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.white))
+        } else {
+            binding.variationTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.secondary_200))
+        }
+
         Glide.with(binding.iconImageView)
             .applyDefaultRequestOptions(requestOptions)
             .load(asset.icon)
             .into(binding.iconImageView)
         binding.symbolTextView.text = asset.symbol
         binding.nameTextView.text = asset.nome
-        binding.priceTextView.text = asset.price.toString()
-        binding. variationTextView.text = asset.variation.toString() + "%"
+        binding.priceTextView.text = priceFormatted
+        binding. variationTextView.text = "$variationValue%"
     }
 }

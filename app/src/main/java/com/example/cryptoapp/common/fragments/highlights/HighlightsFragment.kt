33a39d5606f699844c6ca4ab69305b317161d987
Anderson <anderson.c-morais@hotmail.com>
2023-04-18 18:26:42 -0300

    Change to use android lifecycle

--- app/src/main/java/com/example/cryptoapp/common/fragments/highlights/HighlightsFragment.kt
+++ app/src/main/java/com/example/cryptoapp/common/fragments/highlights/HighlightsFragment.kt
@@ -5,39 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.R
+import com.example.cryptoapp.features.home.HomeRepository
+import com.example.cryptoapp.features.home.HomeViewModel
+import com.example.cryptoapp.features.home.HomeViewModelFactory
 import com.example.cryptoapp.models.HighlightsAdapter
 
 class HighlightsFragment : Fragment() {
-    private lateinit var highlightsAdapter: HighlightsAdapter
+    private val highlightsAdapter = HighlightsAdapter()
+    private lateinit var viewModel: HomeViewModel
 
-    companion object {
-        fun newInstance(highlightsAdapter: HighlightsAdapter): HighlightsFragment {
-            val fragment = HighlightsFragment()
-            fragment.highlightsAdapter = highlightsAdapter
-            return fragment
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        isInitialized()
         val view = inflater.inflate(R.layout.fragment_highlights, container, false)
-
+        viewModel = ViewModelProvider(requireParentFragment(), HomeViewModelFactory(HomeRepository())).get(HomeViewModel::class.java)
         val recyclerView = view.findViewById<RecyclerView>(R.id.highlights_recycler_view)
         recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
         recyclerView.adapter = highlightsAdapter
-
-        return view
-    }
-
-    fun isInitialized() {
-        if(!::highlightsAdapter.isInitialized) {
-            highlightsAdapter = HighlightsAdapter()
+        viewModel.assets.observe(viewLifecycleOwner) { newData ->
+            highlightsAdapter.setAssets(newData)
+            recyclerView.adapter?.notifyDataSetChanged()
         }
+        return view
     }
 }
--- app/src/main/java/com/example/cryptoapp/common/fragments/mostValued/MostValuedFragment.kt
+++ app/src/main/java/com/example/cryptoapp/common/fragments/mostValued/MostValuedFragment.kt
@@ -5,40 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.R
+import com.example.cryptoapp.features.home.HomeRepository
+import com.example.cryptoapp.features.home.HomeViewModel
+import com.example.cryptoapp.features.home.HomeViewModelFactory
 import com.example.cryptoapp.models.MostValuedAdapter
 
 
 class MostValuedFragment : Fragment() {
-    private lateinit var mostValuedAdapter: MostValuedAdapter
-
-    companion object {
-        fun newInstance(mostValuedAdapter: MostValuedAdapter): MostValuedFragment {
-            val fragment = MostValuedFragment()
-            fragment.mostValuedAdapter = mostValuedAdapter
-            return fragment
-        }
-    }
+    private val mostValuedAdapter = MostValuedAdapter()
+    private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        isInitialized()
         val view = inflater.inflate(R.layout.fragment_most_valued, container, false)
-
+        viewModel = ViewModelProvider(requireParentFragment(), HomeViewModelFactory(HomeRepository())).get(HomeViewModel::class.java)
         val recyclerView = view.findViewById<RecyclerView>(R.id.most_valued_recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = mostValuedAdapter
-
-        return view
-    }
-
-    fun isInitialized() {
-        if(!::mostValuedAdapter.isInitialized) {
-            mostValuedAdapter = MostValuedAdapter()
+        viewModel.assets.observe(viewLifecycleOwner) { newData ->
+            mostValuedAdapter.setAssets(newData)
+            recyclerView.adapter?.notifyDataSetChanged()
         }
+        return view
     }
 }

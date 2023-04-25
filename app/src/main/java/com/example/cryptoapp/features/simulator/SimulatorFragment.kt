239d2b7b63106e6a4225e976bd0fa9a8d9a3963a
Anderson <anderson.c-morais@hotmail.com>
2023-04-25 18:00:06 -0300

    Separate viewmodel functions and change text view visibilities

--- app/src/main/java/com/example/cryptoapp/features/simulator/SimulatorFragment.kt
+++ app/src/main/java/com/example/cryptoapp/features/simulator/SimulatorFragment.kt
@@ -8,12 +8,13 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.FragmentSimulatorBinding
 
 class SimulatorFragment : Fragment() {
     private lateinit var _binding: FragmentSimulatorBinding
-    private lateinit var viewModel : SimulatorViewModel
+    private lateinit var viewModel: SimulatorViewModel
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -28,22 +29,84 @@ class SimulatorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupLayout()
+        setupButtonAssetNavigate()
+    }
+
+    private fun setupButtonAssetNavigate() {
+        binding.assetButton.setOnClickListener {
+        }
     }
 
     private fun setupLayout() {
         binding.toolbar.titleTextView.text = getString(R.string.simulator_title)
-
         binding.amountTextInputEditText.addTextChangedListener {
-            viewModel.saveInWallet(binding.amountTextInputEditText.text.toString(), binding.dateTextInputEditText.text.toString())
+            saveAmountAndDate()
+            toggleTextViewsVisibility()
         }
 
         binding.dateTextInputEditText.addTextChangedListener {
-            viewModel.saveInWallet(binding.amountTextInputEditText.text.toString(), binding.dateTextInputEditText.text.toString())
+            saveAmountAndDate()
+            toggleTextViewsVisibility()
         }
 
         viewModel.saveButtonColor.observe(viewLifecycleOwner) { color ->
-            binding.saveInWalletButton.setBackgroundColor(ContextCompat.getColor(requireContext(), color))
-            binding.saveInWalletButton.isEnabled = (color != R.color.primary_300)
+            setSaveButtonColorAndEnable(color)
+            toggleTextViewsVisibility()
+        }
+
+        binding.dateTextInputEditText.addTextChangedListener {
+            priceInDate()
+            toggleTextViewsVisibility()
+        }
+
+        viewModel.datePriceInTittle.observe(viewLifecycleOwner) { date ->
+            setDatePriceInTitle(date)
+            toggleTextViewsVisibility()
+        }
+    }
+
+    private fun saveAmountAndDate() {
+        viewModel.saveInWallet(
+            binding.amountTextInputEditText.text.toString(),
+            binding.dateTextInputEditText.text.toString()
+        )
+    }
+
+    private fun setSaveButtonColorAndEnable(color: Int) {
+        binding.saveInWalletButton.setBackgroundColor(
+            ContextCompat.getColor(
+                requireContext(),
+                color
+            )
+        )
+        binding.saveInWalletButton.isEnabled = (color != R.color.primary_300)
+    }
+
+    private fun priceInDate() {
+        viewModel.priceInDate(binding.dateTextInputEditText.text.toString(), requireContext())
+    }
+
+    private fun setDatePriceInTitle(date: String) {
+        binding.priceInTittleTextView.text = date
+    }
+
+    private fun toggleTextViewsVisibility() {
+        val isAllFieldsFilled = !binding.amountTextInputEditText.text.isNullOrBlank() && !binding.dateTextInputEditText.text.isNullOrBlank()
+
+        if (isAllFieldsFilled) {
+            binding.priceInTittleTextView.visibility = View.VISIBLE
+            binding.priceInTextView.visibility = View.VISIBLE
+            binding.currentPriceTittleTextView.visibility = View.VISIBLE
+            binding.currentPriceTextView.visibility = View.VISIBLE
+            binding.resultPriceTittleTextView.visibility = View.VISIBLE
+            binding.resultPriceTextView.visibility = View.VISIBLE
+        } else {
+            binding.priceInTittleTextView.visibility = View.INVISIBLE
+            binding.priceInTextView.visibility = View.INVISIBLE
+            binding.currentPriceTittleTextView.visibility = View.INVISIBLE
+            binding.currentPriceTextView.visibility = View.INVISIBLE
+            binding.resultPriceTittleTextView.visibility = View.INVISIBLE
+            binding.resultPriceTextView.visibility = View.INVISIBLE
         }
     }
 }
--- app/src/main/java/com/example/cryptoapp/features/simulator/SimulatorViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/features/simulator/SimulatorViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.cryptoapp.features.simulator
 
+import android.content.Context
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -9,15 +11,80 @@ class SimulatorViewModel : ViewModel() {
     private val _saveButtonColor = MutableLiveData<Int>()
     val saveButtonColor: LiveData<Int> = _saveButtonColor
 
+    private val _datePriceInTittle = MutableLiveData<String>()
+    val datePriceInTittle: LiveData<String> = _datePriceInTittle
+
+    private val _resultTittle = MutableLiveData<String>()
+    val resultTittle: LiveData<String> = _resultTittle
+
+    private val _resultColor = MutableLiveData<Int>()
+    val resultColor: LiveData<Int> = _resultColor
+
+    private val _resultPrice = MutableLiveData<String>()
+    val resultPrice: LiveData<String> = _resultPrice
+
+    private val _priceIn = MutableLiveData<String>()
+    val priceIn: LiveData<String> = _priceIn
+
+    private val _currentPrice = MutableLiveData<String>()
+    val currentPrice: LiveData<String> = _currentPrice
+
     init {
         _saveButtonColor.value = R.color.primary_300
     }
 
+    fun priceInDate(date: String, context: Context) {
+        if(date.isNotEmpty()) {
+            val dateIn = context.getString(R.string.simulator_price_in)
+            _datePriceInTittle.value = "$dateIn $date"
+        }
+    }
+
     fun saveInWallet(amount: String, date: String) {
-        if(amount.isNotEmpty() && date.isNotEmpty()) {
+        if(amount.isNotEmpty() && amount != "0" && date.isNotEmpty()) {
             _saveButtonColor.value = R.color.secondary_200
         } else {
             _saveButtonColor.value = R.color.primary_300
+        }
+    }
+
+    fun princeIn(amount: String, value: String, context: Context) {
+        val moneySymbol = context.getString(R.string.simulator_money_symbol)
+        if(amount > 0.0.toString()) {
+            val multiply = amount.toDouble() * value.toDouble()
+            _priceIn.value = "$moneySymbol $multiply"
+        }
+    }
+
+    fun currentPrice(amount: String, value: String, context: Context) {
+        val moneySymbol = context.getString(R.string.simulator_money_symbol)
+        if(amount > 0.0.toString()) {
+            val multiply = amount.toDouble() * value.toDouble()
+            _currentPrice.value = "$moneySymbol $multiply"
+        }
+    }
+
+    fun resultPrice(context: Context) {
+        val moneySymbol = context.getString(R.string.simulator_money_symbol)
+        val priceInValue = _priceIn.value?.substringAfter(moneySymbol)?.trim()?.toDouble() ?: 0.0
+        val currentPriceValue = _currentPrice.value?.substringAfter(moneySymbol)?.trim()?.toDouble() ?: 0.0
+        val result = priceInValue - currentPriceValue
+        _resultPrice.value = "$moneySymbol $result"
+    }
+
+    fun resultColorAndTittle(context: Context) {
+        val resultTittle = context.getString(R.string.simulator_result_price)
+        val resultLoses = context.getString(R.string.simulator_would_lose)
+        val resultProfit = context.getString(R.string.simulator_would_invoice)
+        if(_resultPrice.value!!.toDouble() > 0.0) {
+            _resultColor.value = R.color.green_100
+            _resultTittle.value = "$resultTittle $resultProfit"
+        } else if(_resultPrice.value!!.toDouble().equals(0.0)){
+            _resultColor.value = R.color.tertiary_100
+            _resultTittle.value = "$resultTittle $resultProfit"
+        } else {
+            _resultColor.value = R.color.secondary_100
+            _resultTittle.value = "$resultTittle $resultLoses"
         }
     }
 }
